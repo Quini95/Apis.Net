2c44b4b30faadd1ed4290044ea558f1bfbf13326
Francisco Navarro <francisconavarro725@gmail.com>
2022-09-26 21:36:41 +0200

    Agregar ejercicio 1

---
+++ Ejercicio1/University/universityApiBackend/DataAccess/UniversityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.DataAccess
+{
+    public class UniversityContext: DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options) : base(options)
+        {
+
+        }
+
+        //TODO: Add DbSets(Tables of our Data base)
+
+        public DbSet<User>? Users { get; set; }
+    }
+}
